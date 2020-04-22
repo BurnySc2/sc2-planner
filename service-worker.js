cbd3d55c71b21d797e09a7263ce9ffc739c41870
BurnySc2 <gamingburny@gmail.com>
2020-04-22 18:05:04 +0000

    Deploying to gh-pages from master @ b2e97376431c2e16844a310de7b1f46876a54473 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.45c5649fd93ac7fd8e76843e398a59e8.js"
+  "./precache-manifest.93b688b495ab9c11e823b9487396b3bb.js"
 );
 
 self.addEventListener('message', (event) => {
