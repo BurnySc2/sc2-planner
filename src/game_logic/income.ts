a7b8c6dc3d791747c3499e447167af69c2ee553b
BuRny <gamingburny@gmail.com>
2020-04-28 05:06:41 +0200

    Fix mineral and gas income, add benchmarks and tests

--- src/game_logic/income.ts
+++ src/game_logic/income.ts
@@ -7,30 +7,34 @@ const incomeMinerals = (
     mules: number = 0
 ): number => {
     // Returns mineral income per second.
-    if (workers === 0 && mules === 0 && bases === 0) {
+    if ((workers === 0 && mules === 0) || bases === 0) {
         return 0
     }
+
     // How many close and far mineral patches there are per base
     const amount_close_patches = 5
     const amount_far_patches = 3
     const full_saturation = 3 * (amount_close_patches + amount_far_patches)
+
     // Ignore workers that oversaturate a base (more than 24 workers)
     workers = Math.min(full_saturation * bases, workers)
     if (bases > 1) {
         const w1 = Math.floor(workers / bases)
         const w2 = workers % bases
         const base1Income = incomeMinerals(w1 + w2, 1, mules)
-        const otherBasesIncome = incomeMinerals(w1, bases - 1)
+        const otherBasesIncome = incomeMinerals(w1, 1) * (bases - 1)
         return base1Income + otherBasesIncome
     }
+
     let third_worker_close_patch = 0
     let third_worker_far_patch = 0
     let workers_far_patch = 0
 
-    const income_third_worker_close_patch = 102 - 90
-    const income_third_worker_far_patch = 102 - 78
-    const income_workers_far_patch = 39
-    const income_workers_close_patch = 45
+    const income_workers_far_patch = 32
+    const income_workers_close_patch = 46
+    const income_third_worker_close_patch = 102 - 2 * income_workers_close_patch
+    const income_third_worker_far_patch = 102 - 2 * income_workers_far_patch
+
     // Add workers as third workers to close patches
     if (workers > 19) {
         third_worker_close_patch = workers - 19
@@ -44,8 +48,9 @@ const incomeMinerals = (
         workers_far_patch = workers - 10
         workers -= workers_far_patch
     }
-    // Amount of workers on close patch
+    // Remaining workers are on close patch
     const workers_close_patch = workers
+
     let income_per_min = 0
     const array = [
         workers_close_patch * income_workers_close_patch,
@@ -56,6 +61,7 @@ const incomeMinerals = (
     array.forEach((value) => {
         income_per_min += value
     })
+
     // Income per second
     const mule_rate = 225 / 64
     return (1.4 * income_per_min) / 60 + mules * mule_rate
@@ -76,16 +82,8 @@ const incomeVespene = (workers: number, geysers: number): number => {
         )
     }
     const gas_per_trip = 4
-    const seconds_per_trip_close_gas = [6.3, 2.9, 2.1, 2.1]
-    const seconds_per_trip_far_gas = [7.1, 3.7, 2.4, 2.1]
-    let seconds_per_trip_avg = [0, 1, 2, 3].map((index) => {
-        return (
-            (seconds_per_trip_close_gas[index] +
-                seconds_per_trip_far_gas[index]) /
-            2
-        )
-    })
-    return 1.4 * gas_per_trip * (1 / seconds_per_trip_avg[workers - 1])
+    const seconds_per_trip_close_gas = [6.3, 2.9, 2.07, 2.07]
+    return 1.4 * gas_per_trip * (1 / seconds_per_trip_close_gas[workers - 1])
 }
 
 export { incomeMinerals, incomeVespene }
