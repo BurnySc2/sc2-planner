806f74a1a4c38beaad6ca98f714d4adc20a25a40
BuRny <gamingburny@gmail.com>
2020-09-20 18:47:45 +0200

    Make game version visible https://github.com/BurnySc2/sc2-planner/issues/8

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,8 +5,16 @@ import CLASSES from "../constants/classes"
 export default class Title extends Component {
     render() {
         return (
-            <div className={CLASSES.title}>
-                SC2-Planner - The StarCraft II Build Planner
+            <div>
+                <a
+                    className={CLASSES.title}
+                    href="https://burnysc2.github.io/sc2-planner/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    SC2-Planner - The StarCraft II Build Planner
+                </a>
+                <div className={CLASSES.titleVersion}>For patch 5.0.3</div>
             </div>
         )
     }
