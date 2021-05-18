e4b7bfc9690b360cd675c3f37a2c248d93f77f8e
pikiou <pikiou.sub@gmail.com>
2021-05-18 16:42:41 +0200

    Read start by voice activation

--- src/components/Read.tsx
+++ src/components/Read.tsx
@@ -15,6 +15,7 @@ interface MyState {
     show: boolean
     canStop: boolean
     tooltipText: string | JSX.Element
+    startAtSpeach: boolean
 }
 
 export default class Read extends Component<MyProps, MyState> {
@@ -22,6 +23,8 @@ export default class Read extends Component<MyProps, MyState> {
     voices: SpeechSynthesisVoice[]
     lineHandlers: NodeJS.Timeout[]
     startTime: number
+    listeningToSpeach: boolean
+    recognition: SpeechRecognition | undefined
     /**
      * A small settings menu to enable and disable things
      * And to be able to adjust certain sizes
@@ -34,9 +37,33 @@ export default class Read extends Component<MyProps, MyState> {
             show: false,
             canStop: false,
             tooltipText: "",
+            startAtSpeach: true,
         }
         this.lineHandlers = []
         this.startTime = 0
+
+        this.listeningToSpeach = false
+        const wordList = ["go"]
+        const grammar = `#JSGF V1.0; grammar colors; public <color> = ${wordList.join(" | ")} ;`
+        const anyWindow = window as any
+        let speechRecognitionList: SpeechGrammarList | undefined
+        for (let prefix of ["", "webkit", "moz", "ms", "o"]) {
+            this.recognition =
+                anyWindow[`${prefix}SpeechRecognition`] &&
+                new anyWindow[`${prefix}SpeechRecognition`]()
+            speechRecognitionList =
+                anyWindow[`${prefix}SpeechGrammarList`] &&
+                new anyWindow[`${prefix}SpeechGrammarList`]()
+            if (this.recognition && speechRecognitionList) {
+                speechRecognitionList.addFromString(grammar, 1)
+                this.recognition.grammars = speechRecognitionList
+                this.recognition.continuous = false
+                this.recognition.lang = "en-US"
+                this.recognition.interimResults = false
+                this.recognition.maxAlternatives = 1
+                break
+            }
+        }
     }
 
     showSettings = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -57,15 +84,128 @@ export default class Read extends Component<MyProps, MyState> {
         })
     }
 
-    readBuildOrder(startAt = 0) {
+    wordsToNumber(words: string): number {
+        const small: { [num: string]: number } = {
+            zero: 0,
+            one: 1,
+            two: 2,
+            three: 3,
+            four: 4,
+            five: 5,
+            six: 6,
+            seven: 7,
+            eight: 8,
+            nine: 9,
+            ten: 10,
+            eleven: 11,
+            twelve: 12,
+            thirteen: 13,
+            fourteen: 14,
+            fifteen: 15,
+            sixteen: 16,
+            seventeen: 17,
+            eighteen: 18,
+            nineteen: 19,
+            twenty: 20,
+            thirty: 30,
+            forty: 40,
+            fifty: 50,
+            sixty: 60,
+            seventy: 70,
+            eighty: 80,
+            ninety: 90,
+        }
+
+        let a = words
+            .toString()
+            .toLowerCase()
+            .split(/[\s-]+/)
+        let n = 0
+        let g = 0
+        a.forEach((w) => {
+            var x = /^[0-9]+$/.test(w) ? +w : small[w]
+            if (x != null) {
+                g = g + x
+            } else if (w === "hundred" || w === "100") {
+                g = g * 100
+            }
+        })
+        return n + g
+    }
+
+    startReading(startAt: number) {
         this.setState({
             canStop: true,
         })
-        //TODO1 remove joke
-        //this.speak("Renaud, tu micro comme un ver de terre...", 0, false, "Google français")
-        this.speak("3", 0)
-        this.speak("2", 1)
-        this.speak("1", 2)
+
+        if (!this.state.startAtSpeach || !this.recognition) {
+            this.readBuildOrder(startAt)
+            return
+        }
+        //else
+
+        this.stop()
+        this.recognition.start()
+        this.listeningToSpeach = true
+
+        this.recognition.onresult = (event) => {
+            const voice = event.results[0][0].transcript
+            let startTime = startAt
+            if (/^stop$/.test(voice)) {
+                this.stopReading()
+                this.speak("Stopping")
+            } else if (/^go\b/.test(voice)) {
+                this.stop()
+                const numberWords = voice.replace(/^go\s+/, "")
+                const time = this.wordsToNumber(numberWords)
+                if (time) {
+                    if (time >= 60) {
+                        const minutes = Math.floor(time / 100)
+                        const seconds = time % 100
+                        startTime = minutes * 60 + seconds
+                        this.speak(`${minutes}:${seconds}`)
+                    } else {
+                        startTime = time
+                        this.speak(`${startTime} seconds`)
+                    }
+                }
+                this.readBuildOrder(startTime)
+            }
+        }
+
+        this.recognition.onnomatch = (event) => {
+            console.log("I didn't recognise what was said.")
+            this.recognition?.stop()
+        }
+
+        this.recognition.onerror = (event) => {
+            const error = (event as any).error
+            console.log("Error occurred in recognition: " + error)
+            if (error !== "no-speech") {
+                this.listeningToSpeach = false
+            }
+            this.recognition?.stop()
+        }
+
+        this.recognition.onend = (event) => {
+            if (this.listeningToSpeach) {
+                this.recognition?.start()
+            }
+        }
+    }
+
+    readBuildOrder(startAt = 0) {
+        if (!this.state.startAtSpeach) {
+            this.speak("3", 0)
+            this.speak("2", 1)
+            this.speak("1", 2)
+            this.lineHandlers.push(setTimeout(() => this.readBuildOrderWithoutTimer(startAt), 3000))
+        } else {
+            this.readBuildOrderWithoutTimer(startAt)
+        }
+    }
+
+    readBuildOrderWithoutTimer(startAt = 0) {
         const instructionList: Instruction[] = []
         const lastEvent = this.props.gamelogic.eventLog[this.props.gamelogic.eventLog.length - 1]
         let prevInstruction: Instruction | undefined
@@ -94,25 +234,20 @@ export default class Read extends Component<MyProps, MyState> {
                 }
             }
         }
-        this.lineHandlers.push(
-            setTimeout(() => {
-                instructionList.forEach((instruction) => {
-                    const items = instruction[0]
-                    const count = countBy(items)
-                    const uniqItems = uniq(items)
-                    const itemsWithPlurals = uniqItems.map((item) =>
-                        count[item] === 1 ? item : `${count[item]} ${item}s`
-                    )
-                    const text =
-                        itemsWithPlurals.length === 1
-                            ? itemsWithPlurals[0]
-                            : `${itemsWithPlurals.slice(0, -1).join(", ")}, and ${last(
-                                  itemsWithPlurals
-                              )}`
-                    this.speak(text, instruction[1], instruction[2])
-                })
-            }, 3 * 1000)
-        )
+
+        instructionList.forEach((instruction) => {
+            const items = instruction[0]
+            const count = countBy(items)
+            const uniqItems = uniq(items)
+            const itemsWithPlurals = uniqItems.map((item) =>
+                count[item] === 1 ? item : `${count[item]} ${item}s`
+            )
+            const text =
+                itemsWithPlurals.length === 1
+                    ? itemsWithPlurals[0]
+                    : `${itemsWithPlurals.slice(0, -1).join(", ")}, and ${last(itemsWithPlurals)}`
+            this.speak(text, instruction[1], instruction[2])
+        })
     }
 
     speak(
@@ -148,7 +283,13 @@ export default class Read extends Component<MyProps, MyState> {
         )
     }
 
+    stopReading() {
+        this.listeningToSpeach = false
+        this.stop()
+    }
+
     stop() {
+        this.recognition?.stop()
         if (this.lineHandlers.length) {
             this.lineHandlers.forEach(clearTimeout)
             this.lineHandlers = []
@@ -157,6 +298,12 @@ export default class Read extends Component<MyProps, MyState> {
             })
             this.synth.cancel()
         }
+    }
+
+    onStartAtSpeachChange() {
+        this.setState({
+            startAtSpeach: !this.state.startAtSpeach,
+        })
     }
 
     render() {
@@ -171,6 +318,35 @@ export default class Read extends Component<MyProps, MyState> {
         ) => {
             this.onMouseEnter(e, <div>{tooltip}</div>)
         }
+
+        const speachStart = !this.recognition ? (
+            ""
+        ) : (
+            <div className={CLASSES.dropDownContainer}>
+                <div className={CLASSES.dropDownSubContainer}>
+                    <div
+                        className={CLASSES.dropDownLabel}
+                        data-tip
+                        data-for="readTooltip"
+                        onMouseEnter={mouseEnterFunc(
+                            'Start reading the BO right when you say "Go" in your microphone'
+                        )}
+                    >
+                        Start reading when you say "Go"
+                        <br />
+                        or e.g. "Go 2:15"
+                        <br />
+                        Stop listening at "Stop"
+                    </div>
+                    <input
+                        className={CLASSES.dropDownInput}
+                        type="checkbox"
+                        checked={this.state.startAtSpeach}
+                        onChange={(e) => this.onStartAtSpeachChange()}
+                    />
+                </div>
+            </div>
+        )
 
         const readButton = (
             <div
@@ -204,10 +380,11 @@ export default class Read extends Component<MyProps, MyState> {
                             />
                         </div>
                     </div>
+                    {speachStart}
                     <div className={CLASSES.dropDownContainer}>
                         <div
                             className={CLASSES.dropDownWideButton}
-                            onClick={(e) => this.readBuildOrder(this.startTime)}
+                            onClick={(e) => this.startReading(this.startTime)}
                         >
                             <span className={CLASSES.centeredButton}>Read</span>
                         </div>
@@ -216,7 +393,7 @@ export default class Read extends Component<MyProps, MyState> {
             </div>
         )
         const stopButton = (
-            <div className={stopVisibility} onClick={(e) => this.stop()}>
+            <div className={stopVisibility} onClick={(e) => this.stopReading()}>
                 Stop □
             </div>
         )
