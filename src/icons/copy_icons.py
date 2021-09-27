a4c2004a5631efa0e766a8929e80c40bf19146be
BurnySc2 <gamingburny@gmail.com>
2021-09-27 18:28:17 +0200

    Add and apply pre-commit hook

--- src/icons/copy_icons.py
+++ src/icons/copy_icons.py
@@ -1,19 +1,18 @@
-from typing import List, Set
-from pathlib import Path
-import shutil
-import json
-import os
-
 """
 This small script reads the required icon names from the source files "unit_icons.json" and "upgrade_icons.json" from this directory as well as the customactions.json from the src/constants/ directory.
 
 It tries to find all icons from the "icons_all" subfolder (does not exist by default, the user will have to fill in the icons here) and if they are required by the sc2-planner, they will be copied to the "png" subfolder.
 """
+import json
+import os
+import shutil
+from pathlib import Path
+from typing import Set
 
 folder = Path(__file__).parent
-unit_icons_path = folder / "unit_icons.json"
-upgrade_icons_path = folder / "upgrade_icons.json"
-custom_actions_icons_path = folder.parent / "constants" / "customactions.json"
+unit_icons_path = folder / 'unit_icons.json'
+upgrade_icons_path = folder / 'upgrade_icons.json'
+custom_actions_icons_path = folder.parent / 'constants' / 'customactions.json'
 
 with open(unit_icons_path) as f:
     UNIT_ICONS = json.load(f)
@@ -22,23 +21,23 @@ with open(upgrade_icons_path) as f:
 with open(custom_actions_icons_path) as f:
     CUSTOM_ACTIONS = json.load(f)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Gather all file names
     image_names_to_copy: Set[str] = set()
     image_names_to_copy.update(set(UNIT_ICONS.values()))
     image_names_to_copy.update(set(UPGRADE_ICONS.values()))
-    image_names_to_copy.update(set(d["imageSource"] for d in CUSTOM_ACTIONS))
+    image_names_to_copy.update({d['imageSource'] for d in CUSTOM_ACTIONS})
 
     # Copy files
     path: Path = Path(__file__).parent
-    source_folder = path / "icons_all"
+    source_folder = path / 'icons_all'
     if not source_folder.exists():
         print("'icons_all' subfolder missing. Exiting.")
         exit(1)
 
-    target_folder = path / "png"
+    target_folder = path / 'png'
     if not target_folder.exists():
-        print(f"Target folder {target_folder} did not exist. Creating.")
+        print(f'Target folder {target_folder} did not exist. Creating.')
         os.makedirs(target_folder, exist_ok=True)
 
     for image_name in image_names_to_copy:
@@ -48,7 +47,7 @@ if __name__ == "__main__":
             # print(f"File {image_name} already exists, skipping.")
             pass
         elif source_path.is_file():
-            print(f"Copying {image_name}")
+            print(f'Copying {image_name}')
             shutil.copy(source_path, target_path)
         else:
-            print(f"Missing icon file: {image_name}")
+            print(f'Missing icon file: {image_name}')
