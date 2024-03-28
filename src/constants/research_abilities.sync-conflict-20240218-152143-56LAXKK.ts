13202d9b9fc5bdab1bdacc8e1ea9809856a5e078
burny <gamingburny@gmail.com>
2024-03-28 15:41:01 +0100

    Commit update for patch 5.0.13

---
+++ src/constants/research_abilities.sync-conflict-20240218-152143-56LAXKK.ts
@@ -0,0 +1,27 @@
+import data from "./data.json"
+
+const RESEARCH_ABILITIES: { [name: number]: number } = {}
+
+data.Ability.forEach((ability) => {
+    const target = ability.target
+    if (typeof target !== "string") {
+        const research = target.Research
+        if (research !== undefined) {
+            RESEARCH_ABILITIES[ability.id] = research.upgrade
+            return
+        }
+    }
+})
+
+// Returns object with keys as ability id and value as resulting unit id
+
+// COMMANDCENTERTRAIN_SCV: SCV
+// Exported:
+// {524, 45}
+
+console.assert(
+    Object.keys(RESEARCH_ABILITIES).length === 118,
+    `${Object.keys(RESEARCH_ABILITIES).length} is not 118`
+)
+
+export default RESEARCH_ABILITIES
