2a9c77a4d41505c1c2a262ca36c671b457d10a7c
burnysc2 <gamingburny@gmail.com>
2026-06-01 00:16:49 +0200

    Update researched_by

--- src/constants/researched_by.ts
+++ src/constants/researched_by.ts
@@ -1,64 +1,47 @@
-import RESEARCH_ABILITIES from "./research_abilities"
-import UNITS_BY_ID from "./units_by_id"
-import UPGRADE_BY_ID from "./upgrade_by_id"
+// npx tsx src/constants/researched_by.ts
 import data from "./data.json"
-import { IResearchedBy } from "./interfaces"
+import type { IResearchedBy } from "./interfaces"
 
 const RESEARCHED_BY: IResearchedBy = {}
-data.Unit.forEach((researcherUnit) => {
-    if (researcherUnit.name === "TechLab") {
-        return
-    }
-    researcherUnit.abilities.forEach(
-        // TODO Fix me
-        // @ts-ignore
-        (ability: {
-            ability: number
-            requirements: Array<{ upgrade: number; building: number }>
-        }) => {
-            const resultingUpgradeId = RESEARCH_ABILITIES[ability.ability]
-            const resultingUpgrade = UPGRADE_BY_ID[resultingUpgradeId]
-            const requires: string[] = []
-            if (resultingUpgrade !== undefined) {
-                let requiredStructureId = null
-                let requiredUpgradeId = null
-                if (ability.requirements && Array.isArray(ability.requirements)) {
-                    for (const requirement of ability.requirements) {
-                        if (requirement.upgrade) {
-                            requiredUpgradeId = requirement.upgrade
-                        }
-                        if (requirement.building) {
-                            requiredStructureId = requirement.building
-                        }
-                    }
-                }
-                const requiredStructure =
-                    requiredStructureId !== null ? UNITS_BY_ID[requiredStructureId].name : null
-                if (requiredStructure) {
-                    requires.push(requiredStructure)
-                }
-                const requiredUpgrade =
-                    requiredUpgradeId !== null ? UPGRADE_BY_ID[requiredUpgradeId].name : null
-                if (requiredUpgrade) {
-                    requires.push(requiredUpgrade)
-                }
 
-                // If it doesnt exist: create
-                if (!RESEARCHED_BY[resultingUpgrade.name]) {
-                    RESEARCHED_BY[resultingUpgrade.name] = {
-                        researchedBy: new Set([researcherUnit.name]),
-                        requiredStructure: requiredStructure,
-                        requiredUpgrade: requiredUpgrade,
-                        requires: [[...requires, researcherUnit.name]],
-                    }
-                } else {
-                    // Entry already exists, add training unit to object of 'researchedBy'
-                    RESEARCHED_BY[resultingUpgrade.name].researchedBy.add(researcherUnit.name)
-                }
+const allUpgrades = new Set(Object.keys(data.Upgrades))
+
+for (const [_key, researcherUnit] of Object.entries(data.Units)) {
+    if (researcherUnit.name === "TechLab") {
+        continue
+    }
+    // @ts-expect-error
+    researcherUnit.researches?.forEach(
+        // TODO Fix me
+        (resultingUpgrade: string) => {
+            const researchedByInfo = RESEARCHED_BY[resultingUpgrade] ?? {
+                requiredStructure: null,
+                requiredUpgrade: null,
+                requires: [],
+                researchedBy: new Set(),
             }
-        }
+
+            // @ts-expect-error
+            const upgradeInfo = data.Upgrades[resultingUpgrade]
+            upgradeInfo.requires.forEach((requirement: string) => {
+                if (allUpgrades.has(requirement)) {
+                    researchedByInfo.requiredUpgrade = requirement
+                } else {
+                    researchedByInfo.requiredStructure = requirement
+                }
+            })
+
+            researchedByInfo.researchedBy.add(researcherUnit.name)
+            if (upgradeInfo.requires && 0 < upgradeInfo.requires.length) {
+                researchedByInfo.requires.push(upgradeInfo.requires)
+            }
+
+            if (researchedByInfo.researchedBy.size) {
+                RESEARCHED_BY[resultingUpgrade] = researchedByInfo
+            }
+        },
     )
-})
+}
 
 RESEARCHED_BY["ZergFlyerWeaponsLevel1"].requires = [["Spire"], ["GreaterSpire"]]
 RESEARCHED_BY["ZergFlyerWeaponsLevel2"].requires = [
@@ -91,9 +74,6 @@ RESEARCHED_BY["ZergMeleeWeaponsLevel2"].requires = [
 }
  */
 
-console.assert(
-    Object.keys(RESEARCHED_BY).length === 88,
-    `${Object.keys(RESEARCHED_BY).length} is not 88`
-)
+console.assert(Object.keys(RESEARCHED_BY).length === 89, `${Object.keys(RESEARCHED_BY).length} is not 89`)
 
 export default RESEARCHED_BY
